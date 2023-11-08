1e648eaae72b37dafc4139f251d84189d9e8a616
alexaaqil <alexaaqil.se@gmail.com>
2023-11-08 17:06:30 +0300

    added the models, views and urls for products and categories

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
+from django.db.models import Count
 
 
 def index(request):
-    products = Product.objects.filter(featured=True).order_by("-date")
+    products = Product.objects.filter(product_status="published", featured=True).order_by("-date")
 
     context = {
         "products": products
     }
 
     return render(request, 'core/index.html', context)
+
+
+def product_list_view(request):
+    products = Product.objects.filter(product_status="published").order_by("-date")
+
+    context = {
+        "products": products
+    }
+
+    return render(request, 'core/product_list.html', context)
+
+
+def category_list_view(request):
+    categories = Category.objects.annotate(product_count=Count('product'))
+
+    context = {
+        "categories" : categories
+    }
+
+    return render(request, "core/category_list.html", context)
